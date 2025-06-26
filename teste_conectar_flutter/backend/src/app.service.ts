06dd07b3d88c56b502077070f9c1f4ccd883d47d
Natan Valim Cardoso <natancardosovalim@gmail.com>
2025-06-25 22:19:13 -0300

    Adiciona estrutura inicial do projeto Flutter, incluindo arquivos de configuração, assets e código base para Android e iOS.

--- teste_conectar_flutter/backend/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Sistema de Gerenciamento de Usuários - Conéctar API funcionando!';
-  }
-}
